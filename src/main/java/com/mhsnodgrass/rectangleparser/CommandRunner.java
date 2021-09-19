d18b8cd6f00a6a5c38db274cbf6bd6d404df03eb
MHSnodgrass <matthewsnodgrass@live.com>
2021-09-18 23:19:59 -0300

    #17 - Add Command Line Option For Intersection

--- src/main/java/com/mhsnodgrass/rectangleparser/CommandRunner.java
+++ src/main/java/com/mhsnodgrass/rectangleparser/CommandRunner.java
@@ -18,7 +18,8 @@ public class CommandRunner implements CommandLineRunner {
     /**
      * <p>Takes in the arguments from the command line and checks for options (-p, -h, etc)</p>
      * <p>-p parses the XML file provided as an argument (or uses a default name from application.properties).</p>
-     * <p>-p Calls {@link RectangleParser} to process the file</p>
+     * <p>-i parses the XML file and two ids from the user. Will compare each Rectangle with those ids to see if they intersect. Each argument is needed.</p>
+     * <p>-p and -i Calls {@link RectangleParser} to process the file</p>
      * <p>-h Prints the help output</p>
      * <p>No arguments prints the help output</p>
      * @param args Arguments after an option declaration on the command line, currently looking for the filename of the XML file to be processed
@@ -30,8 +31,13 @@ public class CommandRunner implements CommandLineRunner {
         // Create the parsing option
         Option parse = Option.builder("p")
                 .longOpt("Parse")
-                .desc("Parses XML file into rectangle entities. You can define the filename after the option, or it will use the default from application.properties")
+                .desc("Parses XML file into Rectangle entities and prints them to the output. You can define the filename after the option, or it will use the default from application.properties.")
                 .argName("PARSE")
+                .build();
+        Option intersect = Option.builder("i")
+                .longOpt("Intersect")
+                .desc("Parses XML file into Rectangle entities, and takes two ids. It will compare each Rectangle to see if they intersect. Each argument is required.")
+                .argName("INTERSECT")
                 .build();
         Option help = Option.builder("h")
                 .longOpt("Help")
@@ -39,6 +45,7 @@ public class CommandRunner implements CommandLineRunner {
                 .argName("HELP")
                 .build();
         options.addOption(parse);
+        options.addOption(intersect);
         options.addOption(help);
 
         // Create parser
@@ -55,12 +62,15 @@ public class CommandRunner implements CommandLineRunner {
             // -p
             if (line.hasOption("p")) {
                 rectangleParser.parse(line);
+            // -i
+            } else if (line.hasOption("i")){
+                rectangleParser.intersect(line);
             // -h
             } else if (line.hasOption("h")) {
-                formatter.printHelp("java -jar rectangleparser-0.0.1-SNAPSHOT.jar -p [filename]", options);
+                formatter.printHelp("java -jar rectangleparser-0.0.1-SNAPSHOT.jar [-p <filename>] [-i <filename> <id> <id>]", options);
             // Default help message
             } else {
-                formatter.printHelp("java -jar rectangleparser-0.0.1-SNAPSHOT.jar -p [filename]", options);
+                formatter.printHelp("java -jar rectangleparser-0.0.1-SNAPSHOT.jar [-p <filename>] [-i <filename> <id> <id>]", options);
             }
         } catch (ParseException e) {
             log.error("Error parsing arguments/options", e);
